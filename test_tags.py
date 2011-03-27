83faac0656a78273c3704d61b5f3646176b35760
Ben Osheroff <ben@gimbo.net>
2011-03-27 15:55:33 -0700

    multi-processing in one proc

--- test_tags.py
+++ test_tags.py
@@ -2,6 +2,7 @@
 # coding=UTF-8
 
 import pickle
+import re
 import sys
 import os
 from textrank.ranker import TextRank
@@ -12,9 +13,20 @@ tagger = pickle.load(pickle_file)
 pickle_file.close() 
 ranker = TextRank(tagger)
 
-comments = sys.stdin.read()
-processed = ranker.preprocess(comments)
-keywords = ranker.extract_keywords(processed)    
-for x in keywords:
-  sys.stdout.write("%s - %.2f\n" % (str(x), x.score()))
+comments = ""
+while True:
+    line = sys.stdin.readline()
+    if line == '':
+        break
+
+    if re.match("__TEXTRANK_EOB", line):
+        processed = ranker.preprocess(comments)
+        keywords = ranker.extract_keywords(processed)    
+        for x in keywords:
+          sys.stdout.write("%s - %.2f\n" % (str(x), x.score()))
+        sys.stdout.write("__TEXTRANK_EOB\n")
+        sys.stdout.flush()
+        comments = ""
+    else:
+        comments += line
 
